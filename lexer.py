20c859ffd9ae76a946f4479794ba1336e7a3afbe
Shivam Sarodia <ssarodia@gmail.com>
2015-12-26 17:56:53 -0500

    Created basic lexer

---
+++ lexer.py
@@ -0,0 +1,55 @@
+import re
+
+class Token:
+    def __init__(self, name, info = None):
+        self.name = name
+        self.info = info
+    def __repr__(self):
+        return str(str(self.name) + ((" " + self.info) if self.info  else ""))
+
+class TokenException(Exception):
+    def __init__(self, bad_part):
+        self.bad_part = bad_part
+    def __str__(self):
+        return "Error tokenizing part: " + self.bad_part
+    
+def tokenize(program):
+
+    # A dictionary for the basic primitives
+    prims = {"{":Token("{"),
+             "}":Token("}"),
+             "(":Token("("),
+             ")":Token(")"),
+             "[":Token("["),
+             "]":Token("]"),
+             ";":Token(";"),
+             ",":Token(","),
+             "-":Token("-"),
+             "+":Token("+"),
+             "return":Token("return"),
+             "int":Token("type", "int")}
+
+    parts = re.split("\s+", program) # split the original program by whitespace
+    
+    for prim in prims.keys(): # split by each of the primitives
+        parts = sum([re.split("(" + re.escape(prim) + ")", part) for part in parts], [])
+        parts = [p for p in parts if len(p) > 0]
+            
+    # For each element of parts, tokenize it.
+    def make_token(part):
+        if part in prims.keys(): # tokenize primitives
+            return prims[part]
+        elif re.fullmatch("[0-9]*", part): # tokenize integers
+            return Token("integer", part)
+        elif re.fullmatch("[a-zA-Z_][a-zA-Z0-9_]*", part): # tokenize valid names
+            return Token("name", part)
+        else: # we've found something unexpected! complain.
+            raise TokenException(part)
+
+    return list(map(make_token, parts))
+            
+    
+
+    
+
+        
---
+++ shivc.py
@@ -0,0 +1,43 @@
+import sys
+
+from lexer import *
+
+usage = "Usage: shivc.py <input_c_file> [output_asm_file]"
+
+if __name__=="__main__":
+    
+    # Get arguments and all that icky stuff
+    if len(sys.argv) < 2 or len(sys.argv) > 3:
+        print(usage)
+        sys.exit()
+    elif len(sys.argv) == 2:
+        input_name = str(sys.argv[1])
+        if len(input_name) > 2:
+            output_name = input_name[:-2] + ".s" if input_name[-2:]==".c" else input_name + ".s"
+        else:
+            output_name = input_name + ".s"
+    elif len(sys.argv) == 3:
+        input_name = str(sys.argv[1])
+        output_name = str(sys.argv[2])
+
+    # Open the input file
+    try:
+        f = open(input_name, "r")
+    except:
+        print("Could not open input file. " + usage)
+    else: # if the file opened, try reading it
+        try:
+            program = f.read()
+        except:
+            print("Could not read input file.")
+        else: #if the file opened and was read, then carry on with tokenizing
+            try:
+                tokens = tokenize(program)
+            except TokenException as e: # catch any exceptions from the lexer
+                print(e)
+            else:
+                print(tokens)
+        finally:
+            f.close()
+
+            
